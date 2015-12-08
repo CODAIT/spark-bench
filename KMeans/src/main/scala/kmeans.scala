b940d047ecb72be1755e6683bcd0a663c2728389
MIN LI <minli@us.ibm.com>
2015-12-08 15:13:30 -0800

    License

--- KMeans/src/main/scala/kmeans.scala
+++ KMeans/src/main/scala/kmeans.scala
@@ -1,13 +1,12 @@
-import org.apache.log4j.Logger
+
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * (C) Copyright IBM Corp. 2015 
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0 
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +14,8 @@ import org.apache.log4j.Logger
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import org.apache.spark.mllib.clustering.{KMeansModel, KMeans}
 import org.apache.spark.{SparkContext,SparkConf}
