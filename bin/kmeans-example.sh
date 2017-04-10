77b54bd719b395cfda15bcccf1fba5c9f8a621df
Emily Curtin <emily.curtin@weather.com>
2017-04-10 16:25:02 -0400

    README and example script updates

--- bin/kmeans-example.sh
+++ bin/kmeans-example.sh
@@ -4,12 +4,12 @@ export SPARK_BENCH_JAR=./cli/target/scala-2.11/cli-assembly-2.1.0-SNAPSHOT.jar
 export SPARK_HOME=/opt/spark-2.1.0-bin-hadoop2.6
 export SPARK_MASTER_HOST=local[2]
 
-#rm -rf /tmp/coolstuff
-
-#./bin/spark-bench.sh generate-data -r 5 -c 5 -o /tmp/coolstuff1 --output-format csv kmeans
-#
-#./bin/spark-bench.sh generate-data -r 3000 -c 200 -o /tmp/coolstuff2 --output-format csv kmeans -k 20
-
+rm -rf /tmp/coolstuff1
+rm -rf /tmp/coolstuff2
 rm -rf /Users/ecurtin/Desktop/test-results.csv
+
+./bin/spark-bench.sh generate-data -r 5 -c 5 -o /tmp/coolstuff1 --output-format csv kmeans
+
+./bin/spark-bench.sh generate-data -r 3000 -c 200 -o /tmp/coolstuff2 --output-format csv kmeans -k 20
 
 ./bin/spark-bench.sh workload -i /tmp/coolstuff1 /tmp/coolstuff2 -o /Users/ecurtin/Desktop/test-results.csv kmeans -k 2 32
