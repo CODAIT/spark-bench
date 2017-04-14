059156d94a5ada3413ff704af532d427a01384b1
Emily Curtin <emily@framebit.org>
2017-04-14 10:12:39 -0400

    Probably time to delete the previous version and pull things from it from git history as needed

--- old/ShortestPaths/conf/env.sh
+++
@@ -1,8 +0,0 @@
-# for preparation 
-numV=1000
-NUM_OF_PARTITIONS=10
-mu=4.0
-sigma=1.3
-
-SPARK_STORAGE_MEMORYFRACTION=0.2
-
--- old/StronglyConnectedComponent/conf/env.sh
+++
@@ -1,7 +0,0 @@
-# Application parameters 
-numV=100
-NUM_OF_PARTITIONS=20
-mu=4.0
-sigma=1.3
-
-SPARK_STORAGE_MEMORYFRACTION=0.5
