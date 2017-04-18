d675b2f04d8c4aae9dbcc3fdf889a31e71ec3085
Emily Curtin <emily@framebit.org>
2017-04-18 16:51:09 -0400

    Eh, we don’t need a template file. It’s enough to have the state check in spark-bench.sh

--- bin/spark-bench-env.sh
+++ bin/spark-bench-env.sh
@@ -5,18 +5,15 @@
 # ############################################################### #
 
 export SPARK_HOME= # set this to the directory where Spark is installed in your environment, for example: /opt/spark-spark-2.1.0-bin-hadoop2.6
+
 export SPARK_MASTER_HOST= # set this to the master for your environment, such as local[2], yarn, 10.29.0.3, etc.
 
+########################################################################################################################
 
 
-
-
-
-
-
-# ######################################################################## #
-# It is unlikely that you will need to modify anything below this message  #
-# ######################################################################## #
+# ########################################## #
+# Do not modify anything below this message! #
+# ########################################## #
 
 WHEREILIVE=$(realpath $0)
 BASEDIR=$(dirname $WHEREILIVE)
@@ -25,5 +22,3 @@ PARENTDIR="$(dirname "${BASEDIR}")"
 SB_JAR=`ls ${PARENTDIR}/lib/`
 
 export SPARK_BENCH_JAR=${PARENTDIR}/lib/${SB_JAR}
-
-#TODO: change this to a template. Would make sense to wrap this change into building multiple spark-submits
