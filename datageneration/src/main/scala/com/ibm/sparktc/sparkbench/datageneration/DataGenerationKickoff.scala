2a9b96392570c0a27ed9452606f726082ccd04ea
Emily Curtin <emily@framebit.org>
2017-04-20 16:37:48 -0400

    whoops, forgot to comment this line out when I moved the LinearRegression work to another branch

--- datageneration/src/main/scala/com/ibm/sparktc/sparkbench/datageneration/DataGenerationKickoff.scala
+++ datageneration/src/main/scala/com/ibm/sparktc/sparkbench/datageneration/DataGenerationKickoff.scala
@@ -16,7 +16,7 @@ object DataGenerationKickoff {
   def apply(conf: DataGenerationConf): Unit = {
     conf.generatorName.toLowerCase match {
       case "kmeans" => new KMeansDataGen(conf, spark).run
-      case "linear-regression" => new LinearRegressionDataGen(conf, spark).run()
+//      case "linear-regression" => new LinearRegressionDataGen(conf, spark).run()
       case _ => throw new Exception(s"Unrecognized data generator name: ${conf.generatorName}")
     }
   }
