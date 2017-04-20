9f441b106ca3e19bd87ab49957a680463d095340
Emily Curtin <emily@framebit.org>
2017-04-20 11:26:34 -0400

    Adding linear regression data generation. This is limited in that it can’t write to CSV at the moment.

---
+++ datageneration/src/test/scala/com/ibm/sparktc/sparkbench/LinearRegDataGenTest.scala
@@ -0,0 +1,49 @@
+package com.ibm.sparktc.sparkbench
+
+import java.io.File
+
+import com.holdenkarau.spark.testing.{DataFrameSuiteBase, Utils}
+import com.ibm.sparktc.sparkbench.datageneration.DataGenerationConf
+import com.ibm.sparktc.sparkbench.datageneration.mlgenerator.{KMeansDataGen, LinearRegressionDataGen}
+import com.ibm.sparktc.sparkbench.utils.{KMeansDefaults, LinearRegressionDefaults}
+import com.ibm.sparktc.sparkbench.utils.SparkFuncs.load
+import org.scalatest.{BeforeAndAfterEach, FlatSpec, Matchers}
+
+import scala.io.Source
+
+class LinearRegDataGenTest extends FlatSpec with Matchers with BeforeAndAfterEach with DataFrameSuiteBase {
+  val fileName = s"/tmp/linear-regression/${java.util.UUID.randomUUID.toString}.csv"
+
+  var file: File = _
+
+  override def beforeEach() {
+    file = new File(fileName)
+  }
+
+  override def afterEach() {
+    Utils.deleteRecursively(file)
+  }
+
+  "LinearRegressionDataGen" should "generate data correctly" in {
+
+    val x = DataGenerationConf(
+      generatorName = "linear-regression",
+      numRows = 10,
+      numCols = 10,
+      outputFormat = Some("parquet"),
+      outputDir = fileName,
+      generatorSpecific = Map.empty
+    )
+
+    val generator = new LinearRegressionDataGen(x)
+
+    generator.run()
+
+    val fileList = file.listFiles().toList.filter(_.getName.startsWith("part"))
+    val fileContents = load(spark, fileName, Some("parquet"))
+    val length: Long = fileContents.count()
+
+    length shouldBe x.numRows
+  }
+
+}
--- utils/src/main/scala/com/ibm/sparktc/sparkbench/utils/Defaults.scala
+++ utils/src/main/scala/com/ibm/sparktc/sparkbench/utils/Defaults.scala
@@ -12,6 +12,17 @@ object KMeansDefaults {
   val SEED: Long = 127L
 }
 
+object LinearRegressionDefaults {
+  // Application parameters #1million points have 200M data size
+  val NUM_OF_EXAMPLES: Int = 40000
+  val NUM_OF_FEATURES: Int = 4
+  val EPS: Double = 0.5
+  val INTERCEPTS: Double = 0.1
+  val NUM_OF_PARTITIONS: Int = 10
+  val MAX_ITERATION: Int = 3
+//  val SPARK_STORAGE_MEMORYFRACTION = 0.5
+}
+
 object SomeOtherDefaults{
 
 }
