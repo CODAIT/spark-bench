df2d269fe6b6fc798f11659359ccefe8c8f5a078
Emily Curtin <emily@framebit.org>
2017-05-24 15:11:03 -0400

    might be helpful if this script took args

--- bin/spark-launch.sh
+++ bin/spark-launch.sh
@@ -9,4 +9,4 @@ SB_JAR=`ls ${PARENTDIR}/lib/`
 export SPARK_BENCH_JAR=${PARENTDIR}/lib/${SB_JAR}
 MAIN_CLASS=com.ibm.sparktc.sparkbench.sparklaunch.SparkLaunch
 
-java -cp $SPARK_BENCH_JAR $MAIN_CLASS
+java -cp $SPARK_BENCH_JAR $MAIN_CLASS "$@"
