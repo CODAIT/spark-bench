e1899ded7fa6e253b143fef6735b9a2e79f09c73
Emily Curtin <emily@framebit.org>
2017-05-26 10:16:42 -0400

    launching multiple jvms, I hope

--- spark-launch/src/main/scala/com/ibm/sparktc/sparkbench/sparklaunch/SparkLaunch.scala
+++ spark-launch/src/main/scala/com/ibm/sparktc/sparkbench/sparklaunch/SparkLaunch.scala
@@ -1,9 +1,8 @@
 package com.ibm.sparktc.sparkbench.sparklaunch
 
 import java.io.File
-
 import org.apache.spark.deploy.SparkSubmit
-
+import scala.sys.process._
 import scala.collection.parallel.ForkJoinTaskSupport
 
 object SparkLaunch extends App {
@@ -13,11 +12,14 @@ object SparkLaunch extends App {
     val path = args.head
     val confSeq = SubmitConfigurator(new File(path))
 
+    val sparkHome = sys.env("SPARK_HOME")
+
     val confSeqPar = confSeq.par
     //TODO address the concern that this could be confSeqPar.size threads for EACH member of ParSeq
     confSeqPar.tasksupport = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(confSeqPar.size))
     //TODO nope, this needs to launch new JVMs instead of just calling the method in a new thread
-    confSeqPar.foreach( conf => SparkSubmit.main(conf.toSparkArgs()))
+    confSeqPar.foreach( conf =>  s"""$sparkHome/bin/spark-submit ${conf.toSparkArgs()}""".!)
+
 
   }
 
