9b15cdbcf840eb40181f386d0608e27e26cd0990
Emily Curtin <emily@framebit.org>
2017-05-26 11:14:49 -0400

    tweaking version, tweaking install spark scripts

--- travis-shell-scripts/install-spark.sh
+++ travis-shell-scripts/install-spark.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /opt
 wget https://d3kbcqa49mib13.cloudfront.net/spark-2.1.1-bin-hadoop2.7.tgz
-tar -xvzf spark-2.1.1-bin-hadoop2.7.tgz
+tar -xvzf /opt/spark-2.1.1-bin-hadoop2.7.tgz /opt/
