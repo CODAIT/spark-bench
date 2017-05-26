68855391e35d8e228de12bf995c0adc4d8d55244
Emily Curtin <emily@framebit.org>
2017-05-26 11:02:15 -0400

    moving travis scripts around

---
+++ travis-shell-scripts/install-spark.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd /opt
+wget https://d3kbcqa49mib13.cloudfront.net/spark-2.1.1-bin-hadoop2.7.tgz
+tar -xvzf spark-2.1.1-bin-hadoop2.7.tgz
+export SPARK_HOME=/opt/spark-2.1.1-bin-hadoop2.7.tgz
