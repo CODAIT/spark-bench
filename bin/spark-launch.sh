44ea6e6c025ad746b4f119eefcb1c0d086fc8da9
Emily Curtin <emily@framebit.org>
2017-06-12 17:05:16 -0400

    Fixing bash problems. Still not running cleaning on a cluster, but the issues are now in code not in bash. Progress!

--- bin/spark-launch.sh
+++ bin/spark-launch.sh
@@ -15,29 +15,27 @@ if [ -f $SPARK_HOME/conf/spark-env.sh ]; then
     source $SPARK_HOME/conf/spark-env.sh
 fi
 
-SB_JAR=`ls ${PARENTDIR}/lib`
+SB_JAR=`ls ${PARENTDIR}/lib/ | grep spark-bench-launch`
 
-SPARK_BENCH_JAR=${PARENTDIR}/lib/${SB_JAR}
-MAIN_CLASS=com.ibm.sparktc.sparkbench.sparklaunch.SparkLaunch
+SPARK_BENCH_JAR="${PARENTDIR}/lib/${SB_JAR}"
+MAIN_CLASS="com.ibm.sparktc.sparkbench.sparklaunch.SparkLaunch"
 
-SPARK_BENCH_CLASSPATH+=${SPARK_BENCH_JAR}
+SPARK_BENCH_CLASSPATH="${SPARK_BENCH_JAR}"
 
 if [ -d ${SPARK_HOME}/lib ]; then
-echo Found lib
-SPARK_BENCH_CLASSPATH+=`:${SPARK_HOME}/lib/*`
+    echo Found lib
+    SPARK_BENCH_CLASSPATH+=":${SPARK_HOME}/lib/*"
 fi
 
 if [ -d ${SPARK_HOME}/jars ]; then
-echo Found jars
-SPARK_BENCH_CLASSPATH+=`:${SPARK_HOME}/jars/*`
+    echo Found jars
+    SPARK_BENCH_CLASSPATH+=":${SPARK_HOME}/jars/*"
 fi
 
-echo ----------------------
 echo SPARK_BENCH_CLASSPATH is $SPARK_BENCH_CLASSPATH
 echo MAIN_CLASS is $MAIN_CLASS
-echo ----------------------
 
-java -cp $SPARK_BENCH_CLASSPATH $MAIN_CLASS "$@"
+java -cp "$SPARK_BENCH_CLASSPATH" "$MAIN_CLASS" "$@"
 
 
 
