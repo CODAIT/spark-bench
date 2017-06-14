2d599452e19fe2259e661ff3d4ff4ed6477b78fc
Craig Ingram <cingram@us.ibm.com>
2017-06-14 18:00:25 -0400

    refactored the config wrangler added support for spark-context conf file deletion

--- spark-launch/src/main/scala/com/ibm/sparktc/sparkbench/sparklaunch/SparkLaunch.scala
+++ spark-launch/src/main/scala/com/ibm/sparktc/sparkbench/sparklaunch/SparkLaunch.scala
@@ -1,30 +1,36 @@
 package com.ibm.sparktc.sparkbench.sparklaunch
 
-import java.io.File
-import scala.sys.process._
-import scala.collection.parallel.ForkJoinTaskSupport
 import com.ibm.sparktc.sparkbench.utils.GeneralFunctions.getOrThrow
+import com.typesafe.config._
+import java.io.File
+import scala.collection.parallel.ForkJoinTaskSupport
 import scala.collection.JavaConverters._
-import com.typesafe.config.{Config, ConfigFactory, ConfigObject}
+import scala.sys.process._
 import scala.util.Try
 
 object SparkLaunch extends App {
 
+  private def rmTmpFiles(fns: Seq[String]): Unit = fns.foreach { fn =>
+    Try {
+      val f = new File(fn)
+      if (f.exists) f.delete
+    }
+  }
 
   override def main(args: Array[String]): Unit = {
     println("lol")
     println(s"these are my argz: ${args.mkString(", ")}")
     assert(args.nonEmpty)
     val path = args.head
-    val (confSeq: Seq[SparkLaunchConf], parallel: Boolean ) = mkConfs(new File(path))
-
-    run(confSeq, parallel)
+    val (confSeq: Seq[(SparkLaunchConf, String)], parallel: Boolean) = mkConfs(new File(path))
+    run(confSeq.map(_._1), parallel)
+    rmTmpFiles(confSeq.map(_._2))
   }
 
-  def mkConfs(file: File): (Seq[SparkLaunchConf], Boolean) = {
+  def mkConfs(file: File): (Seq[(SparkLaunchConf, String)], Boolean) = {
     val config: Config = ConfigFactory.parseFile(file)
     val sparkBenchConfig = config.getObject("spark-bench").toConfig
-    val confs: Seq[SparkLaunchConf] = ConfigWrangler(file)
+    val confs: Seq[(SparkLaunchConf, String)] = ConfigWrangler(file)
     val parallel = Try(sparkBenchConfig.getBoolean("spark-contexts-parallel")).getOrElse(false)
     (confs, parallel)
   }
@@ -35,22 +41,21 @@ object SparkLaunch extends App {
   }
 
   def run(confSeq: Seq[SparkLaunchConf], parallel: Boolean): Unit = {
-    if(parallel) {
+    if (parallel) {
       val confSeqPar = confSeq.par
       //TODO address the concern that this could be confSeqPar.size threads for EACH member of ParSeq
       confSeqPar.tasksupport = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(confSeqPar.size))
       confSeqPar.foreach(launch)
-    }
-    else {
-      confSeq.foreach(launch)
-    }
+    } else confSeq.foreach(launch)
   }
 
   def launch(conf: SparkLaunchConf): Unit = {
     val argz: Array[String] = conf.toSparkArgs
     println(s"argz are: ${argz.mkString(", ")}")
     val sparkHome = getOrThrow(sys.env.get("SPARK_HOME"))
-    val returnCode: Int = s"""$sparkHome/bin/spark-submit ${argz.mkString(" ")}""".!
+    val submitCmd = s"""$sparkHome/bin/spark-submit ${argz.mkString(" ")}"""
+    println(s" *** SPARK-SUBMIT: $submitCmd")
+    val returnCode: Int = submitCmd.!
     if (returnCode != 0) {
       throw new Exception(s"spark-submit failed to complete properly given these arguments: \n\t${argz.mkString(" ")}")
     }
--- workloads/src/main/scala/com/ibm/sparktc/sparkbench/workload/MultipleSuiteKickoff.scala
+++ workloads/src/main/scala/com/ibm/sparktc/sparkbench/workload/MultipleSuiteKickoff.scala
@@ -7,33 +7,21 @@ import scala.collection.parallel.ForkJoinTaskSupport
 //TODO this is probably the best place to kickoff the data generation suites and then the workloads
 
 object MultipleSuiteKickoff {
-
-  def run(seq: Seq[MultiSuiteRunConfig]): Unit = {
-    seq.foreach(contextConf => {
-      val spark = createSparkContext()
-      if(contextConf.suitesParallel) {
-        runSuitesInParallel(contextConf.suites, spark)
-      }
-      else {
-        runSuitesSerially(contextConf.suites, spark)
-      }
-    })
+  def run(seq: Seq[MultiSuiteRunConfig]): Unit = seq.foreach { contextConf =>
+    val spark = createSparkContext
+    if (contextConf.suitesParallel) runSuitesInParallel(contextConf.suites, spark)
+    else runSuitesSerially(contextConf.suites, spark)
   }
 
-  def runSuitesInParallel(suiteSeq: Seq[Suite], spark: SparkSession): Unit = {
+  private def runSuitesInParallel(suiteSeq: Seq[Suite], spark: SparkSession): Unit = {
     val parallelSeq = suiteSeq.par
     //TODO address the concern that this could be parallelSeq.size threads for EACH member of ParSeq
     parallelSeq.tasksupport = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(parallelSeq.size))
     parallelSeq.foreach(suite => SuiteKickoff.run(suite, spark))
   }
 
-  def runSuitesSerially(suiteSeq: Seq[Suite], spark: SparkSession): Unit = {
-    suiteSeq.foreach(suite => {
-      SuiteKickoff.run(suite, spark)
-    })
-  }
+  private def runSuitesSerially(suiteSeq: Seq[Suite], spark: SparkSession): Unit =
+    suiteSeq.foreach(SuiteKickoff.run(_, spark))
 
-  def createSparkContext(): SparkSession = {
-    SparkSession.builder().getOrCreate()
-  }
+  private def createSparkContext: SparkSession = SparkSession.builder.getOrCreate
 }
