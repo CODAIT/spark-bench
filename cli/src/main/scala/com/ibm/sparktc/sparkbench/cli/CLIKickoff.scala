70aec628fbcc8ec5dbe20d542ebdcaf9ce2b159b
Matthew Schauer <matthew.schauer@ibm.com>
2017-06-28 16:17:06 -0400

    Removed support for CLI invocation with Scallop

--- cli/src/main/scala/com/ibm/sparktc/sparkbench/cli/CLIKickoff.scala
+++ cli/src/main/scala/com/ibm/sparktc/sparkbench/cli/CLIKickoff.scala
@@ -1,37 +1,15 @@
 package com.ibm.sparktc.sparkbench.cli
 
-import com.ibm.sparktc.sparkbench.datageneration.{DataGenerationConf, DataGenerationKickoff}
-import com.ibm.sparktc.sparkbench.workload.{MultiSuiteRunConfig, MultipleSuiteKickoff, SuiteKickoff}
+import java.io.File
+import com.ibm.sparktc.sparkbench.workload.MultipleSuiteKickoff
 
 object CLIKickoff extends App {
 
-  def startWorkload(scallopArgs: ScallopArgs): Unit = {
-    val sparkContextConfs: MultiSuiteRunConfig = ArgsParser.parseWorkload(scallopArgs)
-    //println("OMFG I'M RUNNING A WORKLOAD")
-    MultipleSuiteKickoff.run(Seq(sparkContextConfs))  }
-
-  def startDataGen(scallopArgs: ScallopArgs): Unit = {
-    val conf: Map[String, Any] = ArgsParser.parseDataGen(scallopArgs)
-    //println("OMFG I'M TOTES GENERATING DATA")
-    DataGenerationKickoff(DataGenerationConf(conf))
-  }
-
-  def useConfFile(sArgs: ScallopArgs): Unit = {
-    //println(sArgs.confFile.apply())
-    val sparkContextConfs = ArgsParser.parseConfFile(sArgs)
-    // We do want this to be serial so that one suite finishes entirely and the next suite starts.
-      MultipleSuiteKickoff.run(sparkContextConfs)
-  }
-
   override def main(args: Array[String]): Unit = {
-//    println(s"hello args! ${args.toSeq}")
-    val sArgs = new ScallopArgs(args)
-
-    sArgs.subcommand match {
-      case Some(sArgs.datagen) => startDataGen(sArgs)
-      case Some(sArgs.workload) => startWorkload(sArgs)
-      case None => useConfFile(sArgs)
-      case _ => new Exception(s"Unrecognized subcommand.\n${sArgs.printHelp()}")
+    args.length match {
+      // We do want this to be serial so that one suite finishes entirely and the next suite starts.
+      case 2 => MultipleSuiteKickoff.run(Configurator(new File(args(1))))
+      case _ => new Exception("Requires exactly one option: config file path")
     }
   }
 }
