d0d4908026f648feb12c003a30c9cd2f70c44ec9
pj.fanning <pj.fanning@workday.com>
2017-10-31 15:33:57 +0000

    Add realpath function

--- bin/spark-bench.sh
+++ bin/spark-bench.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 set -eu
 
+realpath () {
+(
+  TARGET_FILE="$1"
+
+  cd "$(dirname "$TARGET_FILE")"
+  TARGET_FILE="$(basename "$TARGET_FILE")"
+
+  COUNT=0
+  while [ -L "$TARGET_FILE" -a $COUNT -lt 100 ]
+  do
+      TARGET_FILE="$(readlink "$TARGET_FILE")"
+      cd $(dirname "$TARGET_FILE")
+      TARGET_FILE="$(basename $TARGET_FILE)"
+      COUNT=$(($COUNT + 1))
+  done
+
+  echo "$(pwd -P)/"$TARGET_FILE""
+)
+}
+
 bin=$(dirname $(realpath $0))
 basedir=$(dirname "$bin")
 if [[ -d "$basedir/lib" ]]
