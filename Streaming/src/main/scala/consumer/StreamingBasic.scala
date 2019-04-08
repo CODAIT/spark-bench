3696c1e8f921fbe0f86f189922795060bbbc3eb3
qinzhaoyang <qinzhaoyang@inspur.com>
2019-04-08 10:29:59 +0800

    编写接受Kafka流的函数

--- Streaming/src/main/scala/consumer/StreamingBasic.scala
+++ Streaming/src/main/scala/consumer/StreamingBasic.scala
@@ -1,8 +1,14 @@
 package consumer
 
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.log4j.Level
 import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object StreamingBasic {
   def main(args: Array[String]): Unit = {
@@ -11,6 +17,28 @@ object StreamingBasic {
       .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     org.apache.log4j.Logger.getRootLogger.setLevel(Level.WARN)
     conf.registerKryoClasses(Array(classOf[ConsumerRecord[String, String]]))
+    val ssc = new StreamingContext(conf, Seconds(5))
+    val KafkaServerHostsCluster = "10.166.16.35:6667"
+    val consumerGroup = "oneTest"
+    val topicName = "my-output-topic"
+    //ssc.checkpoint("/user/data/checkpoint/streamingTest_1")
+    //ssc.checkpoint("file:///data/spark/checkpoint/streamingTest_1")
+    def getStreamByKafka(ssc: StreamingContext, topic: Array[String], group: String, broker: String)
+    : InputDStream[ConsumerRecord[String, String]] = {
+      //消费者配置
+      val kafkaParam = Map(
+        "bootstrap.servers" -> broker, //用于初始化链接到集群的地址
+        "key.deserializer" -> classOf[StringDeserializer],
+        "value.deserializer" -> classOf[StringDeserializer],
+        "group.id" -> group, //用于标识这个消费者属于哪个消费团体
+        // 如果没有初始化偏移量或者当前的偏移量不存在任何服务器上,可使用这个配置，latest自动重置偏移量为最新的偏移量
+        "auto.offset.reset" -> "latest",
+        "enable.auto.commit" -> (true: java.lang.Boolean) //如果是true，则这个消费者的偏移量会在后台自动提交
+      )
+      //创建DStream，返回接收到的输入数据
+      val stream = KafkaUtils.createDirectStream[String, String](ssc, PreferConsistent, Subscribe[String, String](topic, kafkaParam))
+      stream
+    }
 
   }
 }
