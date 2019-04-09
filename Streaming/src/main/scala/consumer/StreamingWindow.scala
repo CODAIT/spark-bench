d902f93a5d66b88e31fa38c7a25b012b712e84f2
qinzhaoyang <qinzhaoyang@inspur.com>
2019-04-09 14:42:33 +0800

    create spark confs

--- Streaming/src/main/scala/consumer/StreamingWindow.scala
+++ Streaming/src/main/scala/consumer/StreamingWindow.scala
@@ -1,10 +1,18 @@
 package consumer
 
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.spark.SparkConf
+
 /**
   * 对Kafka流进行Window操作的测试
   */
 object StreamingWindow {
   def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+      .setAppName("StreamingKafkaWindowTest")
+      .setMaster("local[2]")
+      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    conf.registerKryoClasses(Array(classOf[ConsumerRecord[String, String]]))
 
   }
 }
