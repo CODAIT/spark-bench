41362082d7f5b69cf9dc846a41c8ad8e7d876122
qinzhaoyang <qinzhaoyang@inspur.com>
2019-04-09 14:56:48 +0800

    change main construction

--- Streaming/src/main/scala/consumer/StreamingBasic.scala
+++ Streaming/src/main/scala/consumer/StreamingBasic.scala
@@ -12,6 +12,24 @@ import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object StreamingBasic {
+
+  def getStreamByKafka(ssc: StreamingContext, topic: Array[String], group: String, broker: String)
+  : InputDStream[ConsumerRecord[String, String]] = {
+    //消费者配置
+    val kafkaParam = Map(
+      "bootstrap.servers" -> broker, //用于初始化链接到集群的地址
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> group, //用于标识这个消费者属于哪个消费团体
+      // 如果没有初始化偏移量或者当前的偏移量不存在任何服务器上,可使用这个配置，latest自动重置偏移量为最新的偏移量
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (true: java.lang.Boolean) //如果是true，则这个消费者的偏移量会在后台自动提交
+    )
+    //创建DStream，返回接收到的输入数据
+    val stream = KafkaUtils.createDirectStream[String, String](ssc, PreferConsistent, Subscribe[String, String](topic, kafkaParam))
+    stream
+  }
+
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf()
       .setAppName("StreamingKafkaTest")
@@ -26,29 +44,10 @@ object StreamingBasic {
 
     //ssc.checkpoint("/user/data/checkpoint/streamingTest_1")
     ssc.checkpoint("file:///data/spark/checkpoint/streamingTest_1")
-
-    def getStreamByKafka(ssc: StreamingContext, topic: Array[String], group: String, broker: String)
-    : InputDStream[ConsumerRecord[String, String]] = {
-      //消费者配置
-      val kafkaParam = Map(
-        "bootstrap.servers" -> broker, //用于初始化链接到集群的地址
-        "key.deserializer" -> classOf[StringDeserializer],
-        "value.deserializer" -> classOf[StringDeserializer],
-        "group.id" -> group, //用于标识这个消费者属于哪个消费团体
-        // 如果没有初始化偏移量或者当前的偏移量不存在任何服务器上,可使用这个配置，latest自动重置偏移量为最新的偏移量
-        "auto.offset.reset" -> "latest",
-        "enable.auto.commit" -> (true: java.lang.Boolean) //如果是true，则这个消费者的偏移量会在后台自动提交
-      )
-      //创建DStream，返回接收到的输入数据
-      val stream = KafkaUtils.createDirectStream[String, String](ssc, PreferConsistent, Subscribe[String, String](topic, kafkaParam))
-      stream
-    }
-
     def getKafkaStreamingRDD(topic: Array[String], consumerGroup: String)
     : InputDStream[ConsumerRecord[String, String]] = {
       getStreamByKafka(ssc, topic, consumerGroup, KafkaServerHostsCluster)
     }
-
     /**
       * ========================缓存数据====================
       **/
